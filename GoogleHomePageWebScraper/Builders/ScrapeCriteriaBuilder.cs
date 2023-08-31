24329cca47b6ccffd3440d4b3bc3472b24a12d06
Leandran <41592532+Leandran@users.noreply.github.com>
2023-08-31 11:48:20 +0200

    Add Data and Builders

---
+++ GoogleHomePageWebScraper/Builders/ScrapeCriteriaBuilder.cs
@@ -0,0 +1,66 @@
+﻿using GoogleHomePageWebScraper.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace GoogleHomePageWebScraper.Builders
+{
+    class ScrapeCriteriaBuilder
+    {
+        // private fields for builder 
+        private string _data;
+        private string _regex;
+        private RegexOptions _regexOption;
+        private List<ScrapeCriteriaPart> _parts;
+            
+        public ScrapeCriteriaBuilder()
+        {
+            setDefaults();
+        }
+
+        private void setDefaults()
+        {
+            _data = string.Empty; // preferred way to delare empty strings
+            _regex = string.Empty;
+            _regexOption = RegexOptions.None;
+            _parts = new List<ScrapeCriteriaPart>(); // newly constructed list
+        }
+
+        public ScrapeCriteriaBuilder WithData(string data)
+        {
+            _data = data;
+            return this;
+        }
+
+        public ScrapeCriteriaBuilder WithRegex(string regex)
+        {
+            _regex = regex;
+            return this;
+        }
+
+        public ScrapeCriteriaBuilder WithRegexOption(RegexOptions regexOption)
+        {
+            _regexOption = regexOption;
+            return this;
+        }
+
+        public ScrapeCriteriaBuilder WithPart(ScrapeCriteriaPart scrapeCriteriaPart)
+        {
+            _parts.Add(scrapeCriteriaPart); // adding to the list
+            return this;
+        }
+
+        public ScrapeCriteria Build()
+        {
+            ScrapeCriteria scrapeCriteria = new ScrapeCriteria();
+            scrapeCriteria.Data = _data;
+            scrapeCriteria.Regex = _regex;
+            scrapeCriteria.RegexOption = _regexOption;
+            scrapeCriteria.Parts = _parts;
+            return scrapeCriteria;
+        }
+    }
+}
---
+++ GoogleHomePageWebScraper/Builders/ScrapeCriteriaPartBuilder.cs
@@ -0,0 +1,50 @@
+﻿using GoogleHomePageWebScraper.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace GoogleHomePageWebScraper.Builders
+{
+    class ScrapeCriteriaPartBuilder
+    {
+        // private fields for builder
+        private string _regex;
+        private RegexOptions _regexOption;
+
+        public ScrapeCriteriaPartBuilder()
+        {
+            setDefaults();
+        }
+
+        private void setDefaults()
+        {
+            _regex = string.Empty; // preferred way to delare empty strings
+            _regexOption = RegexOptions.None;
+        }
+
+        public ScrapeCriteriaPartBuilder WithRegex(string regex)
+        {
+            _regex = regex;
+            return this;
+        }
+
+        public ScrapeCriteriaPartBuilder WithRegexOption(RegexOptions regexOption)
+        {
+            _regexOption = regexOption;
+            return this;
+        }
+
+        public ScrapeCriteriaPart Build()
+        {
+            ScrapeCriteriaPart scrapeCriteriaPart = new ScrapeCriteriaPart();
+            scrapeCriteriaPart.Regex = _regex;
+            scrapeCriteriaPart.RegexOption = _regexOption;
+            return scrapeCriteriaPart;
+
+        }
+
+    }
+}
